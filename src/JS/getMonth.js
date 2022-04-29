b1afd3668a6439738354763b2bd06adfa301fbf1
Walber Melo <dev4wm@gmail.com>
2022-04-29 15:53:09 +0100

    switch month impkement

--- src/JS/getMonth.js
+++ src/JS/getMonth.js
@@ -1,56 +1,70 @@
-const getMonth = ()=>{
-    const gridMonth = document.getElementById('section__div--gridMonth');
-    const currentMonth = document.getElementById('currentMonth');    
-    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December'];    
-    const date = new Date();
-    const initialDay = new Date(date.getFullYear(), 3, 1).getDay();
-    const diasMeses = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+const getMonth = (counter) => {
+  const gridMonth = document.getElementById("section__div--gridMonth");
+  const currentMonth = document.getElementById("currentMonth");
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date();
+  console.log(counter);
+  console.log(date.getMonth() + counter);
+  const initialDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + counter - 1
+  ).getDay();
+  const diasMeses = new Date(
+    date.getFullYear(),
+    date.getMonth() + counter,
+    0
+  ).getDate();
 
-    currentMonth.textContent = `${month[date.getMonth()]}`;  
-    
-    
-    
-    
-    for(let i = 1; i <= diasMeses+initialDay; i++){
-        //Creamos elemento section para el dia
-        const sectionDay = document.createElement('section');
-        //Creamos div para meter el dia
-        const divDay = document.createElement('div');
-        //Crear button para crear eventos
-        const buttonEvent = document.createElement('button');
-        buttonEvent.textContent = "+";
-    
-        sectionDay.classList.add('div__section--days');
-        divDay.classList.add('section__div--singleDay');
-    
-        
-    
-        if(i >= initialDay){
-            sectionDay.setAttribute("id", `${i-initialDay+1}`);
-            divDay.textContent = i-initialDay+1;
-            
-            //darle clase e id a los botones y tab index con setattribute
+  currentMonth.textContent = `${month[date.getMonth()]}`;
+  console.log(initialDay);
+  for (let i = 1; i <= diasMeses + initialDay; i++) {
+    //Creamos elemento section para el dia
+    const sectionDay = document.createElement("section");
+    //Creamos div para meter el dia
+    const divDay = document.createElement("div");
+    //Crear button para crear eventos
+    const buttonEvent = document.createElement("button");
+    buttonEvent.textContent = "+";
 
-            //
-            
-            sectionDay.appendChild(divDay);
-            sectionDay.appendChild(buttonEvent);
-    
-            
-            if( divDay.textContent == date.getDate()){
-                divDay.classList.add('section__div--currentDay');
-            }
+    sectionDay.classList.add("div__section--days");
+    divDay.classList.add("section__div--singleDay");
+    console.log(i - initialDay);
+    debugger;
+    if (i >= initialDay) {
+      sectionDay.setAttribute("id", `${i - initialDay + 1}`);
+      divDay.textContent = i - initialDay + 1;
 
-            if(sectionDay.getAttribute("id") === diasMeses.toString()){
-                i = diasMeses+initialDay
-            }
-        }
-    
-        
-        
-        gridMonth.appendChild(sectionDay);
-    
+      //darle clase e id a los botones y tab index con setattribute
+
+      //
+
+      sectionDay.appendChild(divDay);
+      sectionDay.appendChild(buttonEvent);
+
+      if (divDay.textContent == date.getDate()) {
+        divDay.classList.add("section__div--currentDay");
+      }
+
+      if (sectionDay.getAttribute("id") === diasMeses.toString()) {
+        i = diasMeses + initialDay;
+      }
     }
-}
 
-export{getMonth}
+    gridMonth.appendChild(sectionDay);
+  }
+};
+
+export { getMonth };
--- src/JS/script.js
+++ src/JS/script.js
@@ -1,40 +1,53 @@
 import { getMonth } from "./getMonth.js";
+let counter = 1;
 
-getMonth();
+getMonth(counter);
 
-const buttonHeaderEvent = document.getElementById("headerCreateEvent").addEventListener("click", createEvent);
-const buttonCloseEvent = document.getElementById("button-close--form").addEventListener("click", closeForm);
+const buttonHeaderEvent = document
+  .getElementById("headerCreateEvent")
+  .addEventListener("click", createEvent);
+const buttonCloseEvent = document
+  .getElementById("button-close--form")
+  .addEventListener("click", closeForm);
 const overlay = document.getElementById("blurModal");
-
 
 overlay.addEventListener("click", closeModal);
 
 const form = document.getElementById("mainSectionModal");
 
-function createEvent(){
-    form.classList.toggle("hide");
-    overlay.classList.toggle("hide");
+function createEvent() {
+  form.classList.toggle("hide");
+  overlay.classList.toggle("hide");
 }
 
-function closeForm(){
-    form.classList.toggle("hide");
+function closeForm() {
+  form.classList.toggle("hide");
 }
 
-function closeModal(){
-    form.classList.toggle("hide");
-    overlay.classList.toggle("hide");
+function closeModal() {
+  form.classList.toggle("hide");
+  overlay.classList.toggle("hide");
 }
 
 //CLOSE MODAL WITH ESCAPE KEYBOARD
-document.addEventListener('keydown', function (e){
-  if (e.key === 'Escape' && overlay.classList.contains('hide'));{
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && overlay.classList.contains("hide"));
+  {
     form.classList.add("hide");
     overlay.classList.add("hide");
   }
 
-  if (e.key === 'Enter'&& overlay.classList.contains('hide')){
+  if (e.key === "Enter" && overlay.classList.contains("hide")) {
     form.classList.remove("hide");
     overlay.classList.remove("hide");
   }
-  
 });
+
+const buttonNextMonth = document.getElementById("button-next--month");
+
+buttonNextMonth.addEventListener("click", switchMonth);
+
+function switchMonth() {
+  counter++;
+  getMonth(counter);
+}
